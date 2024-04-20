969fd411507b8506db9bc488da2dcbb4287f4a40
Astrekcz <121060117+Astrekcz@users.noreply.github.com>
2024-04-20 14:00:04 +0200

    Add files via upload

---
+++ src/main/java/com/example/ISS_lampa/IssLampaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISS_lampa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssLampaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssLampaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ISS_lampa/IssLampaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISS_lampa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssLampaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
